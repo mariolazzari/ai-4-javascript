34943b07a8f306d82c5f814fb2c116931f027092
Mario Lazzari <mario.lazzari@gmail.com>
2025-09-02 08:46:34 +0200

    Done chapter 3 lesson 1

--- README.md
+++ README.md
@@ -162,3 +162,49 @@ const hello = async (author,text) => {
 
 hello("Shakespeare", "It was the best of times, it was the worst of times");
 ```
+
+## Incorporating API
+
+### Accrpting user input with readline
+
+```js
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import readline from "readline";
+
+dotenv.config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What do you want to ask to the robot?", async question => {
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a friendly robot",
+      },
+      {
+        role: "user",
+        content: question,
+      },
+    ],
+  });
+
+  console.log(completion.choices[0].message);
+  rl.close();
+});
+```
+
+### Describing image url
+
+```js
+
+```
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import readline from "readline";
 
 dotenv.config();
 
@@ -7,26 +8,26 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const hello = async text => {
-  const stream = await openai.chat.completions.create({
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What do you want to ask to the robot?", async question => {
+  const completion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    stream: true,
-    max_completion_tokens: 128,
     messages: [
       {
         role: "system",
-        content: "You are a world famus author",
+        content: "You are a friendly robot",
       },
       {
         role: "user",
-        content: `Write whis in the style of Shakespeare: ${text}`,
+        content: question,
       },
     ],
   });
 
-  for await (const chunk of stream) {
-    process.stdout.write(chunk.choices[0].delta.content || "");
-  }
-};
-
-hello("It was the best of times, it was the worst of times");
+  console.log(completion.choices[0].message);
+  rl.close();
+});
