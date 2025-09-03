d5743c14503f16b898be116069b6cc205fb15ad4
Mario Lazzari <mario.lazzari@gmail.com>
2025-09-03 08:50:25 +0200

    Done chapter 3

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 import dotenv from "dotenv";
 import axios from "axios";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-import FormData from "form-data";
 
 dotenv.config();
 const apiKey = process.env.OPENAI_API_KEY;
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const recordingPath = path.join(__dirname, "eve-recording.mp3");
-
-const data = new FormData();
-data.append("model", "whisper-1");
-data.append("file", fs.createReadStream(recordingPath));
-
 axios
-  .post("https://api.openai.com/v1/audio/transcriptions", data, {
-    headers: {
-      Authorization: `Bearer ${apiKey}`,
-      "Content-Type": `multipart/form-data;boundary=${data.boundary}`,
+  .post(
+    "https://api.openai.com/v1/images/generations",
+    {
+      model: "dall-e-3",
+      prompt: "A modern milk maid in Vermeer style",
+      size: "1024x1024",
     },
-  })
+    {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    }
+  )
   .then(res => console.log(res.data));
